b64a4a589c13f1e3f969c2298a066d2289794591
narotham112 <narotham.anam@gmail.com>
2017-06-23 20:40:25 -0500

    class adding other examples

--- anr/class_examples.py
+++ anr/class_examples.py
@@ -29,3 +29,46 @@ c = student("Raj",26,"F")
 a.print_student()
 b.print_student()
 c.print_student()
+
+
+print("......................................................")
+'''class phone():
+    def __init__(self):
+        print("hello")
+    def method1(self,model):
+        print(type(model))
+      #  print("model of phone as" + model)
+a = phone()
+a.method1(10)
+'''
+
+
+
+class phone():
+    processor = "amd"
+    color = "Red"
+    size = 7.2
+
+
+    def __init__(self,processor,color,screen_size):
+         self.processor=processor
+         self.color=color
+         self.screen_size=screen_size
+
+    def print_pro(self):
+        print("processor is "+self.processor)
+        print("color is "+self.color)
+        print("screen size is "+str(self.screen_size))
+
+a=phone("Intel","Black",11.2)
+a.print_pro()
+
+
+
+
+
+
+
+
+
+ 
